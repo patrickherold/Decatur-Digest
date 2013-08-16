4cb706755ce6409153116a9a7a2d0584ab6ec17b
Patrick Herold <patrick@herold.org>
2013-08-16 12:22:32 -0400

    fix login routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
 
   def index
   end
-
-
+  
+  def after_sign_in_path_for(resource_or_scope)
+    if request.env['omniauth.origin']
+       request.env['omniauth.origin']
+    end
+  end
 end
