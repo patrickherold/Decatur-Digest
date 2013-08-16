369cc76c41acd9bd42fb3baa21b0777a38372dc5
Patrick Herold <patrick@herold.org>
2013-08-16 12:46:29 -0400

    Fix omni auth in production port

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 DecaturDigest::Application.initialize!
+
+if Rails.env.production?
+  module OmniAuth
+    module Strategy
+      def full_host
+        uri = URI.parse(request.url)
+        uri.path = ''
+        uri.query = nil
+        uri.port = (uri.scheme == 'https' ? 443 : 80)
+        uri.to_s
+      end
+    end
+  end
+end
