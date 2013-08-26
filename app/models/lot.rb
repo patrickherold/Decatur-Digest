f481de21ddefbff2aa56c90adf87536dff157439
Serg Pokrovskyy <neosoftlviv@gmail.com>
2013-08-26 23:54:54 +0300

    Land value deviation view

--- app/models/lot.rb
+++ app/models/lot.rb
@@ -1,8 +1,8 @@
 class Lot < ActiveRecord::Base
 
   attr_accessible :latitude, :longitude, :mailing_latitude, :mailing_longitude, :customer_id, :municipal_id, :name, :property_map_address, :property_street_name
-  geocoded_by :property_map_address   # can also be an IP address
-  after_validation :geocode          # auto-fetch coordinates
+  geocoded_by :property_map_address # can also be an IP address
+  after_validation :geocode # auto-fetch coordinates
   acts_as_gmappable
   has_reputation :votes, source: :user, aggregated_by: :sum
   has_many :portfolios
@@ -11,20 +11,21 @@ class Lot < ActiveRecord::Base
   include ModelStats
 
   UNRANSACKABLE_ATTRIBUTES = ["id", "tax_district", "created_at", "modified_at", "updated_at", "tax_year", "customer_id", "municipal_id", "tax_paid", "tax_dispute"]
+
   def self.ransackable_attributes(auth_object = nil)
-      %w( parcel_id property_map_address owner co_owner appraised_value land_value building_value appeal_value homestead zoning ) + _ransackers.keys
+    %w( parcel_id property_map_address owner co_owner appraised_value land_value building_value appeal_value homestead zoning ) + _ransackers.keys
   end
 
   has_many :lot_votes
 
   def self.by_votes
     select('lots.*, coalesce(value, 0) as votes').
-    joins('left join lot_votes on lot_id=lots.id').
-    order('votes desc')
+        joins('left join lot_votes on lot_id=lots.id').
+        order('votes desc')
   end
-  
+
   scope :commercial_property, where("zoning = ?", 'C3')
-    
+
   scope :appeal_property, where("appeal_value >= ?", '1')
 
   scope :appraised_property, where("appeal_value < ?", '1')
@@ -33,11 +34,18 @@ class Lot < ActiveRecord::Base
 
   scope :all_commericial_appraised, commercial_property.appraised_property
 
+  scope :simlar_land_value, lambda { |base, percent|
+    where(:land_value => (base * (1 - percent))..(base * (1 + percent)))
+  }
+
+  def land_value_deviation(percentage)
+    (Lot.simlar_land_value(land_value, percentage).mean(:land_value) - land_value).abs
+  end
 
   def owner_last_name
     self.owner.split(' ')[0..0].join(' ')
   end
-  
+
   def owner_first_name
     self.owner.split[1..11].join(" ")
   end
@@ -49,7 +57,7 @@ class Lot < ActiveRecord::Base
   def co_owner_last_name
     self.co_owner.split(' ')[0..0].join(' ')
   end
-  
+
   def co_owner_first_name
     self.co_owner.split[1..11].join(" ")
   end
@@ -65,23 +73,23 @@ class Lot < ActiveRecord::Base
   def appraised_appraised
     appraised_value unless appraised_value.nil?
   end
-  
+
   def appraised_currency
     number_to_currency(appraised_appraised, :unit => "$").to_s
   end
-  
+
   def building_appraised
     building_value unless building_value.nil?
   end
-  
+
   def land_appraised
     land_value unless land_value.nil?
   end
-  
+
   def appeal_appraised
     full_appeal unless appeal_value.nil?
   end
-  
+
   def full_appeal
     appeal_value * 2.5
   end
@@ -89,15 +97,15 @@ class Lot < ActiveRecord::Base
   def full_taxable
     taxable * 2.5
   end
-  
+
   def total_tax
     city_tax + school_tax + capital_tax + bond_tax + dda_tax
   end
-  
+
   def appraised_tax
     city_tax_ap + school_tax_ap + capital_tax_ap + bond_tax_ap + dda_tax_ap
   end
-  
+
   def lost_to_appeal
     lost_to_appeal = appraised_tax - total_tax
   end
@@ -118,7 +126,7 @@ class Lot < ActiveRecord::Base
     end
     appraised_appraised * 0.0102 * 0.5
   end
-  
+
   def school_tax_ap
     if general_homestead.present?
       appraised_appraised - school_tax_exemption
@@ -135,7 +143,7 @@ class Lot < ActiveRecord::Base
     end
     appraised_appraised * 0.0209 * 0.5
   end
-  
+
   def capital_tax_ap
     if general_homestead.present?
       appraised_appraised - capital_tax_exemption
@@ -152,7 +160,7 @@ class Lot < ActiveRecord::Base
     end
     appraised_appraised * 0.001 * 0.5
   end
-  
+
   def bond_tax_ap
     if general_homestead.present?
       appraised_appraised - bond_tax_exemption
@@ -169,7 +177,7 @@ class Lot < ActiveRecord::Base
     end
     appraised_appraised * 0.00142 * 0.5
   end
-  
+
   def dda_tax_ap
     if general_homestead.present?
       appraised_appraised - dda_tax_exemption
@@ -224,7 +232,7 @@ class Lot < ActiveRecord::Base
     end
     full_taxable * 0.0102 * 0.5
   end
-  
+
   def school_tax
     if general_homestead.present?
       full_taxable - school_tax_exemption
@@ -241,7 +249,7 @@ class Lot < ActiveRecord::Base
     end
     full_taxable * 0.0209 * 0.5
   end
-  
+
   def capital_tax
     if general_homestead.present?
       full_taxable - capital_tax_exemption
@@ -258,7 +266,7 @@ class Lot < ActiveRecord::Base
     end
     full_taxable * 0.001 * 0.5
   end
-  
+
   def bond_tax
     if general_homestead.present?
       full_taxable - bond_tax_exemption
@@ -275,7 +283,7 @@ class Lot < ActiveRecord::Base
     end
     full_taxable * 0.00142 * 0.5
   end
-  
+
   def dda_tax
     if general_homestead.present?
       full_taxable - dda_tax_exemption
@@ -293,9 +301,9 @@ class Lot < ActiveRecord::Base
     full_taxable * 0.0038 * 0.5
   end
 
- 
-  def city_tax_exemption 
-    if general_homestead.present? 
+
+  def city_tax_exemption
+    if general_homestead.present?
       20000
     elsif senior_62_school_homestead.present?
       70000
@@ -311,9 +319,9 @@ class Lot < ActiveRecord::Base
       0
     end
   end
-     
-  def school_tax_exemption 
-    if general_homestead.present? 
+
+  def school_tax_exemption
+    if general_homestead.present?
       20000
     elsif senior_62_school_homestead.present?
       70000
@@ -329,9 +337,9 @@ class Lot < ActiveRecord::Base
       0
     end
   end
-  
-  def capital_tax_exemption 
-    if general_homestead.present? 
+
+  def capital_tax_exemption
+    if general_homestead.present?
       20000
     elsif senior_62_school_homestead.present?
       70000
@@ -347,27 +355,9 @@ class Lot < ActiveRecord::Base
       0
     end
   end
-  
-  def bond_tax_exemption 
-    if general_homestead.present? 
-      20000
-    elsif senior_62_school_homestead.present?
-      70000
-    elsif senior_62_low_income_school_homestead.present?
-      70000
-    elsif senior_65_school_homestead.present?
-      1000
-    elsif senior_70_school_homestead.present?
-      70000
-    elsif senior_80_school_homestead.present?
-      70000
-    else
-      0
-    end
-  end
- 
-  def dda_tax_exemption 
-    if general_homestead.present? 
+
+  def bond_tax_exemption
+    if general_homestead.present?
       20000
     elsif senior_62_school_homestead.present?
       70000
@@ -383,12 +373,30 @@ class Lot < ActiveRecord::Base
       0
     end
   end
-  
+
+  def dda_tax_exemption
+    if general_homestead.present?
+      20000
+    elsif senior_62_school_homestead.present?
+      70000
+    elsif senior_62_low_income_school_homestead.present?
+      70000
+    elsif senior_65_school_homestead.present?
+      1000
+    elsif senior_70_school_homestead.present?
+      70000
+    elsif senior_80_school_homestead.present?
+      70000
+    else
+      0
+    end
+  end
+
   def no_homestead
     if homestead.nil? || homestead.empty?
     end
   end
-  
+
   def general_homestead
     if homestead == "H1F" || homestead == "LDF" || homestead == "H6DF" || homestead == "H6D" || homestead == "H0"
       city_tax_exemption = 20000
@@ -397,7 +405,7 @@ class Lot < ActiveRecord::Base
       dda_tax = 20000
     end
   end
-  
+
   def senior_62_school_homestead
     if homestead == "H5F"
       city_tax_exemption = 70000
@@ -406,7 +414,7 @@ class Lot < ActiveRecord::Base
       dda_tax = 70000
     end
   end
-  
+
   def senior_62_low_income_school_homestead
     if homestead == "H6F"
       city_tax_exemption = 70000
@@ -415,7 +423,7 @@ class Lot < ActiveRecord::Base
       dda_tax = 70000
     end
   end
-  
+
   def senior_65_school_homestead
     if homestead == "H1"
       city_tax_exemption = 1000
@@ -424,7 +432,7 @@ class Lot < ActiveRecord::Base
       dda_tax = 1000
     end
   end
-  
+
   def senior_70_school_homestead
     if homestead == "H6F" || homestead == "H5F" || homestead == "H1S" || homestead == "H6I" || homestead == "H6IF" || homestead == "H5S"
       city_tax_exemption = 70000
@@ -433,7 +441,7 @@ class Lot < ActiveRecord::Base
       dda_tax = 70000
     end
   end
-  
+
   def senior_80_school_homestead
     if homestead == "H5S"
       city_tax_exemption = @lot.appraised_appraised
@@ -448,7 +456,7 @@ class Lot < ActiveRecord::Base
   end
 
   def gmaps4rails_address
-    "#{self.property_map_address}" 
+    "#{self.property_map_address}"
   end
 
 
@@ -463,7 +471,7 @@ class Lot < ActiveRecord::Base
       [property_street_sub_number, property_street_number, property_street_name_prefix, property_street_name, property_street_type].join(" ")
     end
   end
-  
+
   def property_map_address
     [property_street, property_city, property_state, property_zip].join(", ")
   end
@@ -475,7 +483,7 @@ class Lot < ActiveRecord::Base
       [property_street, property_city, property_state, property_zip].join(", ")
     end
   end
-  
+
   def mailing_street
     if mailing_street_type_postfix.present?
       [mailing_street_number, mailing_street_name_prefix, mailing_street_name, mailing_street_type, mailing_street_type_postfix].join(" ")
@@ -483,7 +491,7 @@ class Lot < ActiveRecord::Base
       [mailing_street_number, mailing_street_name_prefix, mailing_street_name, mailing_street_type].join(" ")
     end
   end
-  
+
   def mailing_map_address
     [mailing_street, mailing_city, mailing_state, mailing_zip].join(", ")
   end
@@ -495,4 +503,5 @@ class Lot < ActiveRecord::Base
       [mailing_street, mailing_city, mailing_state, mailing_zip].join(", ")
     end
   end
+
 end
